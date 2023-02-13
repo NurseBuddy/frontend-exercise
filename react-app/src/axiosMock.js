e81f4c1a39309179efa6ec590411a487079fdb67
Juuso Hämäläinen <juus.h@hotmail.com>
2023-02-13 13:09:51 +0200

    fix indentation

--- react-app/src/axiosMock.js
+++ react-app/src/axiosMock.js
@@ -4,27 +4,27 @@ import MockAdapter from 'axios-mock-adapter';
 const mock = new MockAdapter(axios);
 
 const carers = [
-    { id: 1, name: 'Tom', lastName: 'hanks' },
-    { id: 2, name: 'Harry', lastName: 'Barrie' },
-    { id: 3, name: 'Meghan', lastName: 'Fox' },
-    { id: 4, name: 'Alice', lastName: 'Malice' },
-    { id: 5, name: 'Iris', lastName: 'Boris' },
-    { id: 6, name: 'Omar', lastName: 'Monk' }];
+  { id: 1, name: 'Tom', lastName: 'hanks' },
+  { id: 2, name: 'Harry', lastName: 'Barrie' },
+  { id: 3, name: 'Meghan', lastName: 'Fox' },
+  { id: 4, name: 'Alice', lastName: 'Malice' },
+  { id: 5, name: 'Iris', lastName: 'Boris' },
+  { id: 6, name: 'Omar', lastName: 'Monk' }];
 
 mock.onGet('/carers').reply(200, carers);
 
 mock.onGet(/\/carer\/\d+/).reply((config) => {
-    try {
-        const num = parseInt(config.url.substring('/carer/'.length), 10);
-        const carer = carers.find((c) => c.id === num);
+  try {
+    const num = parseInt(config.url.substring('/carer/'.length), 10);
+    const carer = carers.find((c) => c.id === num);
 
-        if (carer) {
-            return [200, carer];
-        }
-        return [404, { message: 'not found' }];
-    } catch (e) {
-        return [400, { message: 'invalid request' }];
+    if (carer) {
+      return [200, carer];
     }
+    return [404, { message: 'not found' }];
+  } catch (e) {
+    return [400, { message: 'invalid request' }];
+  }
 });
 
 export default axios;
